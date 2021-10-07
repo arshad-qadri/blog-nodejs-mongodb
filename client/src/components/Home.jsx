6b4eecf269789d1094721eedf68a66257a733697
arshad-qadri <arshadqadri321@gmail.com>
2021-10-07 15:45:57 +0530

    designed homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,115 @@
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  FormControl,
+  Button,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
 
 const Home = () => {
+  // console.log(history);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Box bg="gray.100">
+        <Container
+          bg="white"
+          maxW="container.xl"
+          p={{ lg: "40px", md: "40px", sm: "20px" }}
+        >
+          <Box minH="91.6vh">
+            <FormControl>
+              <Input type="text" placeholder="Search here..." />
+            </FormControl>
+            <Box paddingTop="40px">
+              <Heading as="h4" fontSize="1.7rem">
+                What is Lorem Ipsum ?
+              </Heading>
+              <Text marginTop="20px" textAlign="justify">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like... <br />
+                <Button
+                  colorScheme="white"
+                  bg="tomato"
+                  color="#fff"
+                  size="sm"
+                  mt="10px"
+                  letterSpacing="1px"
+                  className="_hover"
+                >
+                  Read more...
+                </Button>
+              </Text>
+            </Box>
+            <Box paddingTop="40px">
+              <Heading as="h4" fontSize="1.7rem">
+                What is Lorem Ipsum ?
+              </Heading>
+              <Text marginTop="20px" textAlign="justify">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like...
+                <br />
+                <Button
+                  colorScheme="white"
+                  bg="tomato"
+                  color="#fff"
+                  size="sm"
+                  mt="10px"
+                  letterSpacing="1px"
+                  className="_hover"
+                >
+                  Read more...
+                </Button>
+              </Text>
+            </Box>
+            <Box paddingTop="40px">
+              <Heading as="h4" fontSize="1.7rem">
+                What is Lorem Ipsum ?
+              </Heading>
+              <Text marginTop="20px" textAlign="justify">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like...
+                <br />
+                <Button
+                  colorScheme="white"
+                  bg="tomato"
+                  color="#fff"
+                  size="sm"
+                  mt="10px"
+                  letterSpacing="1px"
+                  className="_hover"
+                >
+                  Read more...
+                </Button>
+              </Text>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </>
   );
 };
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,92 +8,169 @@ import {
   FormControl,
   Input,
   Center,
-  Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { GoSearch, GoThreeBars } from "react-icons/go";
-
+import { FaSearch } from "react-icons/fa";
 const Navbar = () => {
+  const [show, setShow] = useState("");
+
+  const handleShowMenu = () => {
+    show ? setShow("") : setShow("showMenu");
+  };
   return (
     <>
-      <Box w="100%" py="5" bg="white" boxShadow="lg">
+      <Box w="100%" py="5" bg="white" boxShadow="lg" position="relative">
         <Container maxW="container.xl">
-          <Flex justifyContent="space-between" alignItems="center">
-            <Heading as="h6" fontSize="2rem">
+          <Flex
+            justifyContent={{ lg: "space-between", md: "space-between" }}
+            alignItems={{ lg: "center", md: "center" }}
+            className={` flex-480 ${show}`}
+          >
+            <Heading as="h6" fontSize="2rem" fontStyle="italic">
               <span style={{ color: "tomato" }}>My</span>
               <span>Blog</span>
             </Heading>
 
-            <Flex
-              justifyContent="center"
-              display={{ lg: "block", md: "block", sm: "none" }}
-            >
-              <UnorderedList listStyleType="none">
-                <Flex
-                  justifyContent="center"
-                  alignItems="center"
-                  style={{ columnGap: "30px" }}
-                >
-                  <ListItem fontSize="1.3rem" fontWeight="500">
-                    <NavLink to="/">Home</NavLink>
-                  </ListItem>
-                  <ListItem fontSize="1.3rem" fontWeight="500">
-                    <NavLink to="/about">About</NavLink>
-                  </ListItem>
-                  <ListItem fontSize="1.3rem" fontWeight="500">
-                    <NavLink to="/contact">Contact</NavLink>
-                  </ListItem>
-                </Flex>
-              </UnorderedList>
-              <Box>
-                <FormControl
-                  w="300px"
-                  h="40px"
-                  position="relative"
-                  borderRadius="10px"
-                  overflow="hidden"
-                >
-                  <Input
-                    type="text"
-                    borderRadius="10px"
-                    placeholder="Search..."
-                  />
+            <div className={`menu ${show}`}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  columnGap: "20px",
+                }}
+                className={`${show}`}
+              >
+                <UnorderedList listStyleType="none" marginLeft="0">
                   <Flex
-                    position="absolute"
-                    right="0"
-                    top="50%"
-                    transform="translateY(-50%)"
-                    w="40px"
-                    h="100%"
-                    bg="tomato"
-                    justifyContent="center"
-                    fontSize="1.5rem"
-                    color="white"
-                    fontWeight="600"
+                    w="100%"
+                    // justifyContent={{ lg: "center", md: "center" }}
+                    // alignItems={{
+                    //   lg: "center",
+                    //   md: "center",
+                    // }}
+                    style={{ columnGap: "30px" }}
+                    className={`menu ${show}`}
                   >
-                    <Center>
-                      <GoSearch />
-                    </Center>
+                    <ListItem
+                      fontSize="1.3rem"
+                      fontWeight="500"
+                      className="listItem"
+                    >
+                      <NavLink to="/">Blog</NavLink>
+                    </ListItem>
+                    <ListItem
+                      fontSize="1.3rem"
+                      fontWeight="500"
+                      className="listItem"
+                    >
+                      <NavLink to="/about">About</NavLink>
+                    </ListItem>
+                    <ListItem
+                      fontSize="1.3rem"
+                      fontWeight="500"
+                      className="listItem"
+                    >
+                      <NavLink to="/contact">Contact</NavLink>
+                    </ListItem>
+                    <ListItem
+                      fontSize="1.3rem"
+                      fontWeight="500"
+                      className="listItem"
+                    >
+                      <NavLink to="/contact">Login</NavLink>
+                    </ListItem>
+                    <ListItem
+                      fontSize="1.3rem"
+                      fontWeight="500"
+                      className="listItem"
+                    >
+                      <Box display={{ lg: "block", md: "block", sm: "none" }}>
+                        <Box
+                          bg="tomato"
+                          color="white"
+                          // colorScheme="tomato"
+                          className="btn"
+                          borderRadius="50%"
+                          w="40px"
+                          h="40px"
+                          shadow="xl"
+                          style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          <FaSearch />
+                        </Box>
+                      </Box>
+                    </ListItem>
                   </Flex>
-                </FormControl>
-              </Box>
-            </Flex>
-
+                </UnorderedList>
+                <Box
+                  display={{ lg: "none", md: "none", sm: "block" }}
+                  // w={{ lg: "250px", md: "250px", sm: "100%" }}
+                  width="100%"
+                  mt="10px"
+                >
+                  <FormControl
+                    h="40px"
+                    position="relative"
+                    borderRadius="10px"
+                    overflow="hidden"
+                    className="formControl"
+                  >
+                    <Input
+                      type="text"
+                      borderRadius="10px"
+                      placeholder="Search..."
+                    />
+                    <Flex
+                      position="absolute"
+                      right="0"
+                      top="50%"
+                      transform="translateY(-50%)"
+                      w="40px"
+                      h="100%"
+                      bg="tomato"
+                      justifyContent="center"
+                      fontSize="1.5rem"
+                      color="white"
+                      fontWeight="600"
+                    >
+                      <Center>
+                        <GoSearch />
+                      </Center>
+                    </Flex>
+                  </FormControl>
+                </Box>
+              </div>
+            </div>
             {/* ===================== toggler ===== */}
-            <Flex
-              w="40px"
-              h="40px"
-              //   bg="gray"
-              justifyContent="center"
-              alignItems="center"
-              fontSize="1.8rem"
-              borderRadius="5px"
-              border="solid black"
-              borderWidth="1px"
+            <Box
+              display={{ lg: "none", md: "none", sm: "block" }}
+              onClick={handleShowMenu}
+              position="absolute"
+              right="20px"
             >
-              <GoThreeBars />
-            </Flex>
+              <Box
+                w="40px"
+                h="40px"
+                fontSize="1.8rem"
+                borderRadius="5px"
+                border="solid black"
+                borderWidth="1px"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <GoThreeBars />
+              </Box>
+            </Box>
           </Flex>
         </Container>
       </Box>
